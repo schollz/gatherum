ea59b57ab5221ced879d8b56fe242f9d8172b0b4
Zack Scholl <zack.scholl@gmail.com>
2021-01-01 15:33:32 -0800

    working

--- 1/1.lua
+++ 1/1.lua
@@ -12,14 +12,13 @@ update_ui=false
 
 -- WAVEFORMS
 waveform_samples = {{}}
-waveform_loaded=false
 current_positions={1,1,1,1,1,1}
 
 function init()
-	engine.amp1(0.5)
-	engine.amp2(0.5)
-	engine.amp3(0.25)
-	engine.amp4(0.5)
+	engine.amp1(0.0)
+	engine.amp2(0.0)
+	engine.amp3(0.0)
+	engine.amp4(0.4)
 	engine.amp5(0.2)
 
 
@@ -35,6 +34,17 @@ function init()
   for i=3,6 do
     softcut.rec(i,0)
   end
+
+  -- add params
+  for i=1,3 do 
+    params:add_separator("loop "..i)
+    params:add{ type='binary', name="record",id=i..'rec', behavior='toggle', action=function(v) 
+      softcut.rec_level(i*2-1,v)
+      softcut.rec_level(i*2,v)
+    end 
+    }
+  end
+
 end
 
 function update_positions(i,x)
@@ -49,7 +59,6 @@ end
 
 function on_render(ch, start, i, s)
   waveform_samples[ch] = s
-  waveform_loaded = true 
 end
 
 function reset_softcut()
@@ -88,7 +97,7 @@ function reset_softcut()
     softcut.level_slew_time(i,0.4)
     softcut.rate_slew_time(i,0.4)
 
-    softcut.rec_level(i,1.0)
+    softcut.rec_level(i,0.0)
     softcut.pre_level(i,1.0)
     softcut.position(i,loop_start)
     softcut.phase_quant(i,0.025)
@@ -111,45 +120,57 @@ end
 
 
 function key(k,z)
-
+  redraw()
 end
 
 
 function redraw()
   screen.clear()
   screen.level(15)
-  screen.move(64,32)
-  screen.text_center("hold K1 to load sample")
+  
+  -- show samples
+  screen.level(15)
+  for i=1,3 do 
+    if params:get(i.."rec")==1 then
+      screen.level(15)
+    else
+      screen.level(1)
+    end
+    screen.rect(1+42*(i-1),30,42,64-30)
+    screen.stroke()
+  end
+
+  local waveform_height = 26  
+
   local positions = {}
   for i,p in ipairs(current_positions) do 
     local frac = math.ceil(i/2-1)/3
     positions[i] = util.round(util.linlin(softcut_loop_starts[i],softcut_loop_ends[i],math.ceil(i/2-1)/3*128,math.ceil(i/2)/3*128,p))
   end
-  local waveform_height = 40
-  if waveform_loaded then
-    for i,s in ipairs(waveform_samples[1]) do
-      if i==positions[1] or i==positions[2] or i==positions[3] or i==positions[4] or i==positions[5] or i==positions[6] then 
-        screen.level(15)
-      else
-        screen.level(1)
+
+  if waveform_samples[1] ~=nil and waveform_samples[2] ~=nil then
+    for j=1,2 do 
+      for i,s in ipairs(waveform_samples[j]) do
+        if i==1 or i==1+42 or i==1+42+42 or i>=127 then 
+          goto continue 
+        end
+        if i==positions[1] or i==positions[2] or i==positions[3] or i==positions[4] or i==positions[5] or i==positions[6] then 
+          screen.level(15)
+        else
+          screen.level(1)
+        end
+        local height = util.round(math.abs(s) * waveform_height)
+        screen.move(i,  60-waveform_height/2)
+        screen.line_rel(0, (j*2-3)*height)
+        screen.stroke()
+        ::continue::
       end
-      local height = util.round(math.abs(s) * waveform_height/2)
-      screen.move(i,  64-waveform_height/2)
-      screen.line_rel(0, height)
-      screen.stroke()
     end
-    screen.level(1)
-    for i,s in ipairs(waveform_samples[2]) do
-      if i==positions[1] or i==positions[2] or i==positions[3] or i==positions[4] or i==positions[5] or i==positions[6] then 
-        screen.level(15)
-      else
-        screen.level(1)
-      end
-      local height = util.round(math.abs(s) * waveform_height/2)
-      screen.move(i, 64-waveform_height/2)
-      screen.line_rel(0, -1*height)
-      screen.stroke()
-    end
+  end
+  for i=1,3 do
+    screen.level(15)
+    screen.move(3+42*(i-1),37)
+    screen.text(i)
   end
   screen.update()
 end
