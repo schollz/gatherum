e958b6b4e11a6491899975154e97dee226d13395
Zack Scholl <zack.scholl@gmail.com>
2021-06-08 06:37:24 +0700

    yeah

--- lib/live.lua
+++ lib/live.lua
@@ -1,17 +1,17 @@
--- keep it super stupid 
+-- keep it super stupid
 
--- imports 
+-- imports
 if clock==nil then
-	require("utils")
-	music=require("music")
+  require("utils")
+  music=require("music")
 end
 
 TA={}
 
 function TA:new(o)
-  o = o or {}   -- create object if user does not provide one
-  setmetatable(o, self)
-  self.__index = self
+  o=o or {} -- create object if user does not provide one
+  setmetatable(o,self)
+  self.__index=self
   o.patterns={}
   o.pulse=0
   o.sn=0
@@ -21,193 +21,195 @@ function TA:new(o)
 end
 
 function TA:step()
-	self.pulse = self.pulse+1
-	self.sn=self.sn+1
-	if self.pulse > 16 then 
-		self.pulse=1
-		self.measure = self.measure+1
-	end
-	if self.pulse%4==1 then 
-		self.qn=self.qn+1
-	end
+  self.pulse=self.pulse+1
+  self.sn=self.sn+1
+  if self.pulse>16 then
+    self.pulse=1
+    self.measure=self.measure+1
+  end
+  if self.pulse%4==1 then
+    self.qn=self.qn+1
+  end
 
-	-- emit anything in the time authority
-	for k,v in pairs(self.patterns) do
-		local current=self.measure%#v+1
-		if v[current][self.pulse]~="" then
-			local cmd=v[current][self.pulse]
-			if cmd~=nil then
-				cmd=cmd:gsub("<qn>",self.qn)
-				cmd=cmd:gsub("<sn>",self.sn)
-				print(self.measure+1,self.qn,self.sn,self.pulse,k,cmd)
-				rc(cmd)
-			end
-		end
-	end
+  -- emit anything in the time authority
+  for k,v in pairs(self.patterns) do
+    local current=self.measure%#v+1
+    if v[current][self.pulse]~="" then
+      local cmd=v[current][self.pulse]
+      if cmd~=nil then
+        cmd=cmd:gsub("<qn>",self.qn)
+        cmd=cmd:gsub("<sn>",self.sn)
+        print(self.measure+1,self.qn,self.sn,self.pulse,k,cmd)
+        rc(cmd)
+      end
+    end
+  end
 end
 
 function TA:addm(name,snd,measure)
-    self:add(name,sound(snd,"mp:on('"..name.."',<m>,<sn>)"),measure)
+  self:add(name,sound(snd,"mp:on('"..name.."',<m>,<sn>)"),measure)
 end
 
 -- add row or rows to the time authority for instrument s
 function TA:add(s,t,i)
-	if i~=nil then 
-		self:expand(s,i)
-		if #t[1]==16 and #t>1 then
-			for j, t2 in ipairs(t) do
-				self.patterns[s][i+j-1]=t2
-			end
-		else
-			self.patterns[s][i]=t
-		end
-		do return end
-	end
-	if self.patterns[s]==nil then
-		self.patterns[s]={}
-	end
-	if #t[1]==16 and #t>1 then
-		for _, t2 in ipairs(t) do
-			table.insert(self.patterns[s],t2)
-		end
-	else
-		table.insert(self.patterns[s],t)
-	end
+  if i~=nil then
+    self:expand(s,i)
+    if #t[1]==16 and #t>1 then
+      for j,t2 in ipairs(t) do
+        self.patterns[s][i+j-1]=t2
+      end
+    else
+      self.patterns[s][i]=t
+    end
+    do return end
+  end
+  if self.patterns[s]==nil then
+    self.patterns[s]={}
+  end
+  if #t[1]==16 and #t>1 then
+    for _,t2 in ipairs(t) do
+      table.insert(self.patterns[s],t2)
+    end
+  else
+    table.insert(self.patterns[s],t)
+  end
 end
 
 -- rm will remove instrument s from the time authority
 function TA:rm(s,i)
-	if self.patterns[s]==nil then 
-		do return end
-	end
-	if i~=nil then 
-		if self.patterns[s][i]==nil then 
-			do return end 
-		end
-		self.patterns[s][i]={"","","","","","","","","","","","","","","",""}
-	else
-		self.patterns[s]=nil
-	end
+  if self.patterns[s]==nil then
+    do return end
+  end
+  if i~=nil then
+    if self.patterns[s][i]==nil then
+      do return end
+    end
+    self.patterns[s][i]={"","","","","","","","","","","","","","","",""}
+  else
+    self.patterns[s]=nil
+  end
 end
 
 -- expand will expand instrument s to n rows
 function TA:expand(s,n)
-	if self.patterns[s]==nil then
-		self.patterns[s]={}
-	end
-	for j=1,n do 
-		if self.patterns[s][j]==nil then
-			self.patterns[s][j]={"","","","","","","","","","","","","","","",""}
-		end
-	end
+  if self.patterns[s]==nil then
+    self.patterns[s]={}
+  end
+  for j=1,n do
+    if self.patterns[s][j]==nil then
+      self.patterns[s][j]={"","","","","","","","","","","","","","","",""}
+    end
+  end
 end
 
 -- rc runs any code, even stupid code
 function rc(code)
-	local ok,f=pcall(load(code))
-	if ok then
-		if f~=nil then
-			f()
-		end
-	else
-		print(string.format("rc: could not run '%s': %s",code,f))
-	end
+  local ok,f=pcall(load(code))
+  if ok then
+    if f~=nil then
+      f()
+    end
+  else
+    print(string.format("rc: could not run '%s': %s",code,f))
+  end
 end
 
 -- returns an euclidean spaced array of "item"
-function er(item,num,size) 
-	if size==nil then 
-		size=16
-	end
-	local ray={}
-	local bucket=0
-	for i=1,size do
-		ray[size+1-i]="" 
-		bucket=bucket+num 
-		if bucket >= size then 
-			bucket=bucket-size
-			ray[size+1-i]=item
-		end
-	end
-	return ray
+function er(item,num,size)
+  if size==nil then
+    size=16
+  end
+  local ray={}
+  local bucket=0
+  for i=1,size do
+    ray[size+1-i]=""
+    bucket=bucket+num
+    if bucket>=size then
+      bucket=bucket-size
+      ray[size+1-i]=item
+    end
+  end
+  return ray
 end
 
 
 -- adds two arrays
 function add(t,t2)
-	local t3={}
-	for i,v1 in ipairs(t) do
-		local v2=t2[i]
-		if v1~="" then
-			table.insert(t3,v1)
-		else 
-			table.insert(t3,v2)
-		end
-	end
-	return t3
+  local t3={}
+  for i,v1 in ipairs(t) do
+    local v2=t2[i]
+    if v1~="" then
+      table.insert(t3,v1)
+    else
+      table.insert(t3,v2)
+    end
+  end
+  return t3
 end
 
 -- subtract two arrays
 function sub(t,t2)
-	local t3={}
-	for i,v1 in ipairs(t) do
-		local v2=t2[i]
-		if v1~="" and v2~="" then
-			table.insert(t3,"")
-		else
-			table.insert(t3,v1)
-		end
-	end
-	return t3
+  local t3={}
+  for i,v1 in ipairs(t) do
+    local v2=t2[i]
+    if v1~="" and v2~="" then
+      table.insert(t3,"")
+    else
+      table.insert(t3,v1)
+    end
+  end
+  return t3
 end
 
 -- rotates an array by amt
 function rot(t,amt)
-	local rotated={}
-	for i=#t-amt+1,#t do
-		table.insert(rotated,t[i])
-	end
-	for i=1,#t-amt do
-		table.insert(rotated,t[i])
-	end
-	return rotated
+  local rotated={}
+  for i=#t-amt+1,#t do
+    table.insert(rotated,t[i])
+  end
+  for i=1,#t-amt do
+    table.insert(rotated,t[i])
+  end
+  return rotated
 end
 
 
 function sound(s,ctx)
-	local rays={}
-	local lines = string.split(s,";")
-	for i,line in ipairs(lines) do
-		local words=string.split(line," ")
-		local ray=er("-",#words)
-		local cmds={}
-		for j,word in ipairs(words) do
-			local notes=music.to_midi(word)
-			local cmd=""
-			for _, note in ipairs(notes) do
-				for _, ctxn in ipairs({"m","v","f","n"}) do 
-					if string.find(ctx,"<"..ctxn..">") then
-						cmd=cmd..ctx:gsub("<"..ctxn..">",note[ctxn])..";"
-					end
-				end
-			end
-			table.insert(cmds,cmd)
-			
-		end
-		local k=1
-		for j,rayw in ipairs(ray) do
-			if rayw=="-" then
-				ray[j]=cmds[k]
-				k=k+1
-			end
-		end
-		table.insert(rays,ray)
-	end
-	if #rays==1 then 
-		return rays[1]
-	else
-		return rays
-	end
+  local rays={}
+  local lines=string.split(s,";")
+  for i,line in ipairs(lines) do
+    local words=string.split(line," ")
+    local ray=er("-",#words)
+    local cmds={}
+    for j,word in ipairs(words) do
+      local cmd=""
+      if word~="." then
+        local notes=music.to_midi(word)
+        for _,note in ipairs(notes) do
+          for _,ctxn in ipairs({"m","v","f","n"}) do
+            if string.find(ctx,"<"..ctxn..">") then
+              cmd=cmd..ctx:gsub("<"..ctxn..">",note[ctxn])..";"
+            end
+          end
+        end
+      end
+      table.insert(cmds,cmd)
+
+    end
+    local k=1
+    for j,rayw in ipairs(ray) do
+      if rayw=="-" then
+        ray[j]=cmds[k]
+        k=k+1
+      end
+    end
+    table.insert(rays,ray)
+  end
+  if #rays==1 then
+    return rays[1]
+  else
+    return rays
+  end
 end
 
 
@@ -220,7 +222,7 @@ ta=TA:new()
 -- add kick on 2nd measure
 -- ta:add("kick",r(e("print('kick',<qn>)",4),0),1)
 -- for i=1,(2*16) do
--- 	ta:step()
+-- ta:step()
 -- end
 
 -- table.print(r(e("print('kick')",1),4))
--- livecoding.lua
+++ livecoding.lua
@@ -9,33 +9,67 @@
 -- time authority   ta:rm ta:add(name,er,index)
 
 
+-- tock
+
+params:set("clock_tempo",168)
 norns.script.load("code/gatherum/live.lua")
 sched:start()
+sched:stop()
+
+ta:rm("op1")
+ta:expand("op1",8)
+ta:addm("op1","G/B:3",1)
+ta:addm("op1","G/D:4",2)
+ta:addm("op1","Bm:3",3)
+ta:addm("op1","Bm/F#:4",4)
+ta:addm("op1","F#m/A:3",5)
+ta:addm("op1","F#m/C#:4",6)
+ta:addm("op1","Amaj:3",7)
+ta:addm("op1","Amaj/E:4",8)
+
+ta:rm("usb")
+ta:addm("usb","b2 d3 b2 a2 f#3 g4 g4 a4",1)
+ta:addm("usb","d3 e3 f#4 a4 g4 f#4 b1 a1",2)
+ta:addm("usb","b2 . . . b2 . . .  e3 . d3 . f#3 . g3 .",1)
+ta:addm("usb",". d3 .  e3  d3  . f#1  . . g1 . . ",2)
+ta:addm("usb","b2",1)
+ta:addm("usb","d2",2)
 
 ta:rm("kick")
-ta:add("kick",er("kick:hit()",1),2)
-ta:add("kick",add(er("kick:hit()",2),rot(er("kick:hit()",1),3)),1)
+ta:add("kick",er("kick:hit()",2),1)
+ta:add("kick",add(er("kick:hit()",2),rot(er("kick:hit()",1),3)),2)
+
+e.d_amp(0.03)
+ta:add("drone",sound("d5","e.d_midi(<m>)"),1)
+ta:add("drone",sound("b4","e.d_midi(<m>)"),4)
+
+e.bb_load("/home/we/dust/audio/breakbeat/breakbeat_168bpm_4beats.wav",clock.get_tempo(),168)
+e.bb_amp(0.00)
+ta:add("bb",er("if math.random()<0.5 then e.bb_sync((<sn>-1)%64/64) end",4))
+ta:add("bbb",er("if math.random()<0.1 then; v=math.random(); e.bb_break(v,v+math.random()/40+0.01) end",4),1)
+
+juststop()
+juststart()
+
 
 ta:rm("hh")
-ta:add("hh",er("hh:hit()",15),1)
+ta:add("hh",er("hh:hit()",6),1)
 ta:add("hh",sub(er("hh:hit()",15),er("hh:hit()",4)),2)
 ta:add("hh",sub(er("hh:hit()",15),er("hh:hit()",2)),3)
 
-kick.patch.distAmt=8; kick.patch.oscDcy=500
-kick.patch.level=-10; hh.patch.level=-10
+kick.patch.distAmt=5; kick.patch.oscDcy=500
+kick.patch.level=-5; hh.patch.level=-5
 e.bl()
 
 juststop()
 juststart()
 
-e.d_amp(0.00)
-ta:add("drone",sound("c4","e.d_midi(<m>)"),1)
 ta:add("drone",sound("a3","e.d_midi(<m>)"),2)
 ta:add("drone",sound("e4","e.d_midi(<m>)"),3)
 ta:add("drone",sound("d4","e.d_midi(<m>)"),4)
 
 e.bb_load("/home/we/dust/code/gatherum/data/breakbeats_160bpm2_4beats.wav",clock.get_tempo(),160)
-e.bb_amp(0.00)
+e.bb_amp(0.1)
 ta:rm("bb")
 ta:add("bb",er("if math.random()<0.5 then e.bb_sync((<sn>-1)%64/64) end",4))
 ta:rm("bbb")
