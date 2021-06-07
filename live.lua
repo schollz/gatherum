9f698529c08b8ba041a1509674b2ad3fbac3fd41
Zack Scholl <zack.scholl@gmail.com>
2021-06-07 07:18:47 -0700

    ok

--- live.lua
+++ live.lua
@@ -6,7 +6,20 @@ engine.name="IDLive"
 require("live/lib/utils")
 music=require("live/lib/music")
 require("live/lib/live")
+lattice=require("lattice")
 
 function init()
-
+  local sched=lattice:new{
+    ppqn=16
+  }
+  sched:new_pattern({
+    action=function(t)
+      print(t)
+      tva:step()
+    end,
+    division=1/4,
+    enabled=true
+  })
 end
+
+ 
