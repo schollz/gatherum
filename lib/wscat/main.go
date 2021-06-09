731c4c3603012de45582ba6014755d43492507a0
Zack Scholl <zack.scholl@gmail.com>
2021-06-09 14:24:41 -0700

    wscat options

--- lib/wscat/main.go
+++ lib/wscat/main.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"net/url"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/gorilla/websocket"
 )
 
+var flagAddr = flag.String("addr", "localhost", "address")
+var flagPort = flag.String("port", "5555", "port")
+var flagSubProtocol = flag.String("sub", "bus.sp.nanomsg.org", "sub protocol")
+var flagSend = flag.String("send", "<wscat>", "format sent piped input")
+
+// wscat --send 'last_command=[[<wscat>]];<wscat>'
 func main() {
-	u := url.URL{Scheme: "ws", Host: "localhost:5555", Path: "/"}
+	flag.Parse()
+	u := url.URL{Scheme: "ws", Host: *flagAddr + ":" + *flagPort, Path: "/"}
 	var cstDialer = websocket.Dialer{
-		Subprotocols:     []string{"bus.sp.nanomsg.org"},
+		Subprotocols:     []string{*flagSubProtocol},
 		ReadBufferSize:   1024,
 		WriteBufferSize:  1024,
 		HandshakeTimeout: 3 * time.Second,
@@ -24,7 +33,9 @@ func main() {
 	}
 	defer norns.Close()
 	stdin := bufio.NewScanner(os.Stdin)
+	s := ""
 	for stdin.Scan() {
-		norns.WriteMessage(websocket.TextMessage, []byte("last_command=[["+stdin.Text()+"]];"+stdin.Text()+"\n"))
+		s += stdin.Text()
 	}
+	norns.WriteMessage(websocket.TextMessage, []byte(strings.Replace(*flagSend, "<wscat>", s, 1)+"\n"))
 }
