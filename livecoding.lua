fb525e729a5a23ec66031b84adbd8f69222199ab
Zack Scholl <zack.scholl@gmail.com>
2021-06-10 09:02:51 +0700

    more

--- livecoding.lua
+++ livecoding.lua
@@ -10,12 +10,13 @@ nature(0.0)
 
 tapebreak()
 tapestop()
+tapestart()
 
 -- closer
 params:set("clock_tempo",120)
 
 e.s_load(1,"/home/we/dust/audio/live/closer.wav"); 
-e.s_amp(1,0.5); 
+e.s_amp(1,0); 
 e.s_mov(1,3/28)
 e.s_mov(1,26/28)
 ta:expand("closer",64)
@@ -32,6 +33,9 @@ play("crow","db4",3)
 play("crow",". eb4",5)
 play("crow","gb4",7)
 play("crow","gb4",8)
+
+
+play("crow","ab5",1)
 
 stop("crow")
 crow.output[2].action="{ to(10,0),to(0,0.07) }"; crow.output[2]()
@@ -64,16 +68,17 @@ play("geo","b2  e1 e1 . b2  . g# b b2  e1 e1 . b2  . g# b",2)
 play("geo","gb1 gb db . gb3 . b  db gb1 gb db . gb3 . b  db",3)
 play("geo","eb1 eb bb . bb .  ab bb eb1 eb bb . bb .  ab bb",4)
 
-e.bb_load("/home/we/dust/audio/breakbeat/bpm120/beats8_bpm120_adt_120_drum_break_leak.wav",clock.get_tempo(),120)
+e.bb_load("/home/we/dust/audio/breakbeat/bpm120/beats8_bpm120_adt_120_drum_break_vinylised.wav",clock.get_tempo(),120)
 e.bb_load("/home/we/dust/audio/breakbeat/bpm120/beats8_bpm120_rb_drum_loop_break_byrd_120.wav",clock.get_tempo(),120)
 e.bb_load("/home/we/dust/audio/breakbeat/bpm120/beats8_bpm120_rb_drum_loop_break_duke_120.wav",clock.get_tempo(),120)
 e.bb_load("/home/we/dust/audio/breakbeat/bpm120/beats8_bpm120_rb_drum_loop_break_west_120.wav",clock.get_tempo(),120)
 e.bb_load("/home/we/dust/audio/breakbeat/bpm120/beats8_bpm120_rb_drum_loop_break_pult_120.wav",clock.get_tempo(),120)
-e.bb_amp(0.5)
+e.bb_amp(0.0)
 ta:rm("bb")
 play("bb",er("if math.random()<0.5 then e.bb_sync((<sn>-1)%64/64) end",4))
 
-tapebreak()
+clock.run(function() clock.sleep(1); tapestop(); clock.sleep(3); tapestart() end)
+clock.run(function() clock.sleep(1); tapebreak() clock.sleep(4); tapebreak() end)
 tapestop()
 tapestart()
 
@@ -133,6 +138,17 @@ play("op1","F#m/C#:4",6)
 play("op1","Amaj:3",7)
 play("op1","Amaj/E:4",8)
 
+stop("op1)
+play("op1","f#5 . e5 c#5 f#5 . e5 c#5",1)
+play("op1","f#5 . e5 c#5 f#5 . e5 c#5",2)
+play("op1","f#5 . d5 c#5 f#5 . d5 c#5",3)
+play("op1","f#5 . d5 c#5 f#5 . d5 c#5",4)
+play("op1","a5 . d5 c#5 a5 . d5 c#5",5)
+play("op1","a5 . d5 c#5 a5 . d5 c#5",6)
+play("op1","b5 . e5 c#5 b5 . e5 c#5",7)
+play("op1","b5 . e5 c#5 b5 b5 b5 b5",8)
+
+
 stop("geo")
 play("geo","b2 d3 b2 a2 f#3 g4 g4 a4",1)
 play("geo","d3 e3 f#4 a4 g4 f#4 b1 a1",2)
@@ -147,6 +163,7 @@ play("kick",er_add(er(2),rot(er(1),3)),2)
 kick.patch.level=-1
 stop("hh")
 play("hh",er_sub(er(16),er(4)),1)
+play("hh",er_sub(er(13),er(4)),1)
 hh.patch.level=1
 
 stop("sd")
@@ -155,6 +172,7 @@ sd.patch.distAmt=58; sd.patch.level=-5; sd.patch.nEnvDcy=220;
 stop("clap")
 play("clap",rot(er(2),4),1)
 clap.patch.level=0
+
 
 stop("crow")
 crow.output[2].action="{ to(10,0),to(0,0.05) }"; crow.output[2]()
@@ -167,10 +185,11 @@ play("sh","a5 c# e c# e5 e a6 c# ",4)
 crow.output[2].action="{ to(10,2),to(0,6) }"; crow.output[2]()
 crow.output[3].action="lfo(3.1415,10)"; crow.output[3]()
 stop("crow")
+ta:expand("crow",8)
 play("crow","d3",1)
 play("crow","f#3",3)
-play("crow","f#3 g",5)
-play("crow","b2",8)
+play("crow","a3",5)
+play("crow","e4",7)
 play("crow","b2",9)
 play("crow","c#4",10)
 play("crow","e4",12)
@@ -179,11 +198,15 @@ play("crow","a3",14)
 tapestop()
 tapestart()
 tapebreak()
+e.bb_amp(0.7)
 
 e.bb_load("/home/we/dust/audio/breakbeat/breakbeat_168bpm_4beats.wav",clock.get_tempo(),168)
 e.bb_load("/home/we/dust/audio/live/breakbeat168bpm.wav",clock.get_tempo(),168)
 e.bb_load("/home/we/dust/audio/live/breakbeat_165bpm.wav",clock.get_tempo(),165)
-e.bb_amp(0.6)
+e.bb_load("/home/we/dust/audio/breakbeat/bpm165/beats8_bpm165_Rope_Break_165_PL.wav",clock.get_tempo(),165)
+e.bb_load("/home/we/dust/audio/breakbeat/bpm165/beats8_bpm165_Duplex_Break_165_PL.wav",clock.get_tempo(),165)
+e.bb_load("/home/we/dust/audio/breakbeat/bpm165/beats8_bpm165_Absorb_Break_165_PL.wav",clock.get_tempo(),165)
+e.bb_amp(0.0)
 play("bb",er("if math.random()<0.5 then e.bb_sync((<sn>-1)%64/64) end",4))
 play("bbb",er("if math.random()<0.1 then; v=math.random(); e.bb_break(v,v+math.random()/40+0.01) end",4),1)
 
