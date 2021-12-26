bf1d23f2a2d32c21b258a93d6d08c4e710a73899
Zack Scholl <zack.scholl@gmail.com>
2021-12-26 10:28:09 -0800

    add link-test

---
+++ link-test.lua
@@ -0,0 +1,35 @@
+-- link est
+lattice_=require("lattice")
+beat=0
+function init()
+
+  lattice=lattice_:new()
+  pattern=lattice:new_pattern{
+    action=function(t)
+      beat=math.floor(t/96)+1
+    end,
+    division=1/4,
+  }
+  lattice:start()
+
+  clock.run(function()
+    while true do
+      clock.sleep(1/15)
+      redraw()
+    end
+  end)
+end
+
+function key(k,z)
+  if z==1 then
+    lattice:hard_restart()
+  end
+end
+
+function redraw()
+  screen.clear()
+  screen.font_size(24)
+  screen.move(64,32)
+  screen.text_center(beat)
+  screen.update()
+end
